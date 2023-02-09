2744b66020c8a01056f9aa335a8c999da2308d44
Mike Mccullough <mikesawesomevids@gmail.com>
2023-02-09 13:12:38 -0500

    Minotaur rigged

--- Labyrinth/Assets/Scripts/PlayerMovement.cs
+++ Labyrinth/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,61 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
-    public float moveSpeed = 5f;
-    // TODO: Need a minimap and to update it when the player moves and the minotaur is within a certain distance
-    // TODO: Need to create methods, variables, and classes to handle any logic that would relate to the player movement
+     private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
+    private float xRot;
+    private float yRot;
 
-    // Probably a good idea to add a MiniMap class to handle the minimap logic 
+    [SerializeField] private Transform PlayerCamera;
+    [SerializeField] private Rigidbody Playerbody;
+    [Space]
+    [SerializeField] private float Speed;
+    [SerializeField] private float Sensitivity;
+    //Code for Jumping
+    //[SerializeField] private float Jumpforce;
 
-    private void MovePlayer() {
-        // Player movement logic
-        // Use transform.Translate to move the player
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement
+        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        //camera
+        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        MovePlayer();
+        MoveCamera();
+
+    }
+
+    //code affects how the player moves
+    private void MovePlayer()
+    {
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
+        Playerbody.velocity = new Vector3(MoveVector.x, Playerbody.velocity.y, MoveVector.z);
+
+        /* Jumping code
+         if(Input.GetKeyDown(Keycode.Space))
+        {
+            Playerbody.Addforce(Vector3.up * Jumpforce, Forcemode.Impulse);
+        }
+         */
+    }
+
+    //code affects how the camera moves
+    private void MoveCamera()
+    {
+        xRot -= PlayerMouseInput.y * Sensitivity;
+
+        transform.Rotate(0f, PlayerMouseInput.x * Sensitivity, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
     }
 
 
