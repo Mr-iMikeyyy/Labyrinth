9da785cc0e5fa38ac005d25184e42a639e22338f
Arcivan <ihuang@wccnet.edu>
2023-04-23 19:54:47 -0400

    WIP SEEDS

---
+++ Labyrinth/Assets/Scripts/Algorithms/Seeds/SeedFormulas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SeedFormulas
+{
+    private static string base36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static uint SeedNum;
+    public static string Seed;
+
+    public static int ConvertToSeed(string seed)
+    {
+        Seed = seed;
+        for (int x = seed.Length - 1; 0 <= x; x--)
+        {
+
+        }
+    }
+
+}
