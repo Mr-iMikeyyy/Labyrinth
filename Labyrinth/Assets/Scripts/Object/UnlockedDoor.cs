c2ff26223b7fc144b9ffbdc4990ea1afe4bc34ad
Arcivan <ihuang@wccnet.edu>
2023-05-06 19:56:21 -0400

    changed door script (UNTESTED IN MAZE GEN, WORKS IN HUDTEST)

--- Labyrinth/Assets/Scripts/Object/UnlockedDoor.cs
+++ Labyrinth/Assets/Scripts/Object/UnlockedDoor.cs
@@ -4,24 +4,61 @@ using UnityEngine;
 
 public class UnlockedDoor : Interactable
 {
-    [SerializeField] public string keyType;
     [SerializeField] private Animator doorAnimator;
-    private Collider thiscoll;
+    [SerializeField] private Collider Parentcollider;
+    [SerializeField] private GameObject parent;
+    [SerializeField] private GameObject navmesh;
+    [SerializeField] private Animation op, cl;
     public AudioSource doorOpenSound;
-
+    private bool doorOpen;
+    private float timesincelastpress = 0;
+    private bool inProcess;
     void Start()
     {
-        if (keyType == null)
-        {
-            keyType = "White";
-        }
-        thiscoll = GetComponent<Collider>();
+        doorOpen = false;
+        inProcess = false;
     }
 
+    private void Update()
+    {
+        timesincelastpress += Time.deltaTime;
+    }
     override protected void Interact()
     {
-        doorAnimator.Play("door opening");
-        doorOpenSound.Play();
-        thiscoll.enabled = false;
+        if (inProcess == false)
+        {
+            inProcess = true;
+            if (doorOpen == false && timesincelastpress > 1)
+            {
+                Debug.Log("open");
+                Parentcollider.enabled = false;
+                doorAnimator.Play("door opening");
+                doorOpenSound.Play();
+                timesincelastpress = 0;
+                changeDoor();
+            }
+            else if (doorOpen == true && timesincelastpress > 1)
+            {
+                Debug.Log("close");
+                Parentcollider.enabled = true;
+                doorAnimator.Play("door closing");
+                doorOpenSound.Play();
+                timesincelastpress = 0;
+                changeDoor();
+            }
+            inProcess = false;
+        }
+    }
+
+    private void changeDoor()
+    {
+        if (doorOpen == true)
+        {
+            doorOpen = false;
+        }
+        else
+        {
+            doorOpen = true;
+        }
     }
 }
